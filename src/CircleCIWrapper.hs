3dfc8842ccd33936b2fb458b2554a61eec61fbbd
즈눅 <xnuk@users.noreply.github.com>
2019-01-16 18:04:37 +0000

    Create CircleCIWrapper.hs

---
+++ src/CircleCIWrapper.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module CircleCIWrapper where
+
+baseUrl :: String
+baseUrl = "https://circleci.com/api/v1.1"
+
+circleOption :: Text -> Options
+circleOption token = defaults & header "Accept" .~ ["application/json"] & param "circle-token" .~ [token]
+
+circleGet token url = getWith (circleOption token) (baseUrl ++ url)
+circleTrigger token url = postWith (circleOption token) (baseUrl ++ url) mempty
